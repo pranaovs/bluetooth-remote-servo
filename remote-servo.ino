ea1525552160a21b9b1178edc1583fd64be74ffe
Pranaov S <148376495+pranaovs@users.noreply.github.com>
2025-07-08 13:17:33 +0530

    refactor: add logging

--- remote-servo.ino
+++ remote-servo.ino
@@ -5,11 +5,12 @@
 #define OFF_POS 0
 #define ON_POS 10
 Servo myservo;
-bool servoState = true;  // Global servo state (on or off position)
+bool servoState = false;  // Global servo state (on or off position)
 
 // Button variables
 #define BUTTON_PIN 2
-#define BUTTON_DELAY_MS 1000  // Time to freeze execution after button press
+#define BUTTON_RELEASE_CHECK_MS 10  // Time to wait to check if button is released
+#define BUTTON_DELAY_MS 2000  // Time to wait after button press
 
 // Setup Logic
 void setup() {
@@ -17,8 +18,10 @@ void setup() {
   Serial.begin(9600);
   Serial.println("Started");
 
-  myservo.attach(MYSERVO_PIN);
   pinMode(BUTTON_PIN, INPUT_PULLUP);
+
+  myservo.attach(MYSERVO_PIN);
+  myservo.write(OFF_POS);
 
   Serial.println("Initialized!");
 }
@@ -28,21 +31,33 @@ void loop() {
   bool buttonState = digitalRead(BUTTON_PIN);
 
   if (buttonState == LOW) {  // LOW because INPUT_PULLUP
-    button_press();
-    delay(BUTTON_DELAY_MS);
+    button_press(buttonState);
   }
 }
 
 /* Function called when the button is pressed
- * Arguments: None
+ * Arguments: bool (state)
  * Returns: None
+ * Note: blocks code execution until button is released
  */
-void button_press() {
-  Serial.println("Button ");
+void button_press(bool state) {
+  Serial.print("Button ");
   Serial.print(BUTTON_PIN);
   Serial.println(" pressed");
 
+  // Button press action
   toggle_servo();
+
+  // Button release logic
+  while (digitalRead(BUTTON_PIN) == state) {
+    delay(BUTTON_RELEASE_CHECK_MS);
+  }
+
+  Serial.print("Button ");
+  Serial.print(BUTTON_PIN);
+  Serial.println(" released");
+
+  delay(BUTTON_DELAY_MS);
 }
 
 /*
@@ -66,8 +81,7 @@ bool toggle_servo() {
 void switch_servo(bool state) {
   if (state == true) {
     myservo.write(ON_POS);
-  }
-  else {
+  } else {
     myservo.write(OFF_POS);
   }
 
